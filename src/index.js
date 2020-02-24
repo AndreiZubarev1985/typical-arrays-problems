0697ec13cde42299ee4d401b5a6762975c71e9a0
Andrei <andreizubarev1985@gmail.com>
2020-02-24 15:51:28 +0300

    feat: my solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+ if(Array.isArray(array) && array.length) {
+  let result = array.sort( (a, b) => {
+    if(a > b) return 1;
+    if(a < b) return -1;
+    if(a == b) return 0;
+});
+let res = result.splice(0, 1); 
+return +res;
+ } else {
+   return 0;
+ }
 }
 
+
 exports.max = function max (array) {
+  if(Array.isArray(array) && array.length) {
+  let result = array.sort( (a, b) => {
+    if(a > b) return 1;
+    if(a < b) return -1;
+    if(a == b) return 0;
+});
+let res = result.pop(); 
+return +res;
+} else {
   return 0;
+}
 }
 
 exports.avg = function avg (array) {
+  if(Array.isArray(array) && array.length) {
+  let result = array.reduce((sum, current) => sum + current, 0);
+   return result/20;
+} else {
   return 0;
+}
 }
